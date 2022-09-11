eff0185f471003ab69171120777a71d951b34e7d
danielsteman <daniel-steman@live.nl>
2022-09-11 20:50:58 +0200

    added readvoltage

---
+++ ReadVoltage/ReadVoltage.ino
@@ -0,0 +1,42 @@
+// Code from https://how2electronics.com/iot-based-battery-status-monitoring-system-using-esp8266/
+int analogInPin  = A0;    // Analog input pin
+int sensorValue;          // Analog Output of Sensor
+float calibration = -0.12; // Check Battery voltage using multimeter & add/subtract the value
+int bat_percentage;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Start");
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  sensorValue = analogRead(analogInPin);
+  float voltage = (((sensorValue * 3.3) / 1024) * 2 + calibration); //multiply by two as voltage divider network is 100K & 100K Resistor
+ 
+  bat_percentage = mapfloat(voltage, 2.8, 4.2, 0, 100); //2.8V as Battery Cut off Voltage & 4.2V as Maximum Voltage
+ 
+  if (bat_percentage >= 100)
+  {
+    bat_percentage = 100;
+  }
+  if (bat_percentage <= 0)
+  {
+    bat_percentage = 1;
+  }
+ 
+  Serial.print("Analog Value = ");
+  Serial.print(sensorValue);
+  Serial.print("\t Output Voltage = ");
+  Serial.print(voltage);
+  Serial.print("\t Battery Percentage = ");
+  Serial.println(bat_percentage);
+  delay(1000);
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
